ac6584ad419ef1aee06ba122aea88361a2216707
thedb <thedb11@gmail.com>
2026-01-14 17:43:02 +0100

    Adding the aarch64-darwin system and cleaning up.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Hytale";
+  description = "A simple flake for the Hytale launcher.";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -7,57 +7,107 @@
 
   outputs =
     { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib)
+        optionals
+        ;
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem = nixpkgs.lib.genAttrs systems;
+    in
     {
-    packages =
-      let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-        target = "linux/amd64";
-
-        hytale-launcher = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
-          name = "hytale-launcher";
-          version = "2026.01.14-563c3d7";
-
-          src = pkgs.fetchurl {
-            url = "launcher.hytale.com/builds/release/${target}/hytale-launcher-${finalAttrs.version}.flatpak";
-            sha256 = "sha256-LthZo0XOTYFlbeO8syxcaslNn0a0BLrRBCTAiIqJfOk=";
+      packages = forEachSupportedSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
           };
 
-          nativeBuildInputs = [
-            pkgs.ostree
-            pkgs.autoPatchelfHook
-          ];
+          hytale-launcher = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
+            name = "hytale-launcher";
+            version = "2026.01.14-563c3d7";
 
-          buildInputs = pkgs.lib.optionals pkgs.stdenvNoCC.isLinux [
-            pkgs.webkitgtk_4_1
-          ];
+            src = pkgs.fetchurl (
+              let
+                systemSpecific =
+                  {
+                    x86_64-linux = {
+                      path = "linux/amd64";
+                      extension = "flatpak";
+                    };
+                    aarch64-darwin = {
+                      path = "darwin/arm64";
+                      extension = "dmg";
+                    };
+                  }
+                  .${system};
+              in
+              {
+                url =
+                  "launcher.hytale.com/builds/release/${systemSpecific.path}"
+                  + "/hytale-launcher-${finalAttrs.version}.${systemSpecific.extension}";
+                sha256 = (import ./hashes.nix).${system};
+              }
+            );
 
-          dontUnpack = true;
+            nativeBuildInputs =
+              optionals pkgs.stdenv.isLinux [
+                pkgs.ostree
+                pkgs.autoPatchelfHook
+              ]
+              ++ optionals pkgs.stdenv.isDarwin [
+                pkgs.undmg
+              ];
 
-          # https://github.com/flatpak/flatpak/issues/126#issuecomment-227068860
-          installPhase = ''
-            mkdir $out
-            ostree init --repo=repo --mode=archive-z2
-            ostree static-delta apply-offline --repo=repo $src
-            ostree checkout --repo=repo -U $(basename $(echo repo/objects/*/*.commit | cut -d/ -f3- --output-delimiter= ) .commit) outdir
-            cp -r outdir/files/* $out/
+            buildInputs = pkgs.lib.optionals pkgs.stdenvNoCC.isLinux [
+              pkgs.webkitgtk_4_1
+            ];
 
-            # Get rid of the 'hytale-launcher-wrapper' functionality. We don't
-            # need any auto-updating or flatpak shenanigans going on.
-            echo "exec $out/bin/hytale-launcher" > $out/bin/hytale-launcher-wrapper
-          '';
+            dontUnpack = true;
 
-          meta.mainProgram = "hytale-launcher";
-        });
-      in
-      {
-        ${system} = {
+            installPhase =
+              if pkgs.stdenv.isLinux then
+                # https://github.com/flatpak/flatpak/issues/126#issuecomment-227068860
+                ''
+                  mkdir $out
+                  ostree init --repo=repo --mode=archive-z2
+                  ostree static-delta apply-offline --repo=repo $src
+                  ostree checkout --repo=repo -U $(basename $(echo repo/objects/*/*.commit | cut -d/ -f3- --output-delimiter= ) .commit) outdir
+                  cp -r outdir/files/* $out/
+
+                  # Get rid of the 'hytale-launcher-wrapper' functionality. We don't
+                  # need any auto-updating or flatpak shenanigans going on.
+                  echo "exec $out/bin/hytale-launcher" > $out/bin/hytale-launcher-wrapper
+                ''
+              else if pkgs.stdenv.isDarwin then
+                ''
+                  mkdir $out/{Applications,bin}
+                  undmg $src
+                  cp ./"Hytale Launcher.app" $out/Applications/
+                  cp ./"Hytale Launcher.app"/Contents/MacOS/hytale-launcher $out/bin/
+                ''
+              else
+                throw "Unsupported system.";
+
+            meta = {
+              description = "The Hytale launcher.";
+              homepage = "https//hytale.com";
+              license = pkgs.lib.licenses.unfreeRedistributable;
+              mainProgram = "hytale-launcher";
+              platforms = systems;
+              sourceProvenance = [ pkgs.lib.sourceTypes.binaryNativeCode ];
+            };
+          });
+        in
+        {
           inherit hytale-launcher;
           default = hytale-launcher;
-        };
-      };
-  };
+        }
+      );
+
+      formatter = forEachSupportedSystem (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+    };
 }
---
+++ hashes.nix
@@ -0,0 +1,4 @@
+{
+  x86_64-linux = "sha256-LthZo0XOTYFlbeO8syxcaslNn0a0BLrRBCTAiIqJfOk=";
+  aarch64-darwin = "sha256-GgkH98wXa63vsVDzDgJ3MZIiN3iqfXuoWU+/PHD4oEw=";
+}
